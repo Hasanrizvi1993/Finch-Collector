a24b1f31b00943311e8119f72dcb1f4bf4062503
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-03-26 10:44:55 -0700

    added cars

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,25 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class Car:
+    def __init__(self, name, year, color):
+        self.name = name
+        self.year = year
+        self.color = color
+
+cars = [
+    Car("Ford", 2011, "Red"),
+    Car("Toyota", 2008, "Blue"),
+    Car("Suzuki", 1984, "Green"),
+    Car("Honda", 2020, "Yellow"),
+]
+
+class CarList(TemplateView):
+    template_name = 'carlist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cars"] = cars # this is where we add the key into our context object for the view to use
+        return context
+
