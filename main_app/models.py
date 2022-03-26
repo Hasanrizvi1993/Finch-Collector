3f36d6abfef5cbd97ddc9164d6fa12ecbf56db2d
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-03-26 15:26:13 -0700

    added user to the models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Car(models.Model):
@@ -7,9 +8,11 @@ class Car(models.Model):
     year = models.IntegerField()
     color = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
+
